a25da23b2feb0479a78df36bdeae864c9cd688ef
AbdRaqeeb <Ajaorqb@gmail.com>
2020-04-10 17:56:02 +0100

    Updates

---
+++ precache-manifest.998298e2214da4faa9705e16263e034b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b398ada6d1f7802ea7fb8d313e0227d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae416c1f2aa0919779bd",
+    "url": "/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "3455e529129869e45340",
+    "url": "/static/css/main.40f88196.chunk.css"
+  },
+  {
+    "revision": "ae416c1f2aa0919779bd",
+    "url": "/static/js/2.622c8221.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.622c8221.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3455e529129869e45340",
+    "url": "/static/js/main.3b9624eb.chunk.js"
+  },
+  {
+    "revision": "7f582fa61a09db9e7908",
+    "url": "/static/js/runtime-main.ca1bfcaf.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
